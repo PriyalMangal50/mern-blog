7a9977f84c51c531e0025feebde1f0d10bbb51ec
PriyalMangal50 <145255577+PriyalMangal50@users.noreply.github.com>
2025-09-18 13:54:20 +0530

    Add support for multiple allowed origins

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const app = express();
 
 // Allowed origins
+
 const allowedOrigins = process.env.FRONTEND_URL
   ? process.env.FRONTEND_URL.split(",")
   : ["http://localhost:5173"];
